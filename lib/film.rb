74852d9b3bb1f75db9b8654b6ba54b24d687948f
Lemeri02 <9588504@gmail.com>
2019-11-04 17:05:31 +0500

    final version

---
+++ lib/film.rb
@@ -0,0 +1,20 @@
+class Film
+  attr_reader :title, :director, :year
+
+  def self.from_file(data_path)
+    data_path.map do |film|
+      content = File.readlines(film, encoding: 'UTF-8', chomp: true)
+      new(content[0], content[1], content[2])
+    end
+  end
+
+  def initialize(title, director, year)
+    @title = title
+    @director = director
+    @year = year.to_i
+  end
+
+  def to_s
+    "#{director} - #{title} (#{year})"
+  end
+end
--- main.rb
+++ main.rb
@@ -1,7 +1,29 @@
 # Программа "Фильм на вечер"
-#
-#
-#
-#
-#
-#
+
+require_relative 'lib/film'
+
+files = Dir["#{__dir__}/data/*.txt"]
+
+films = Film.from_file(files)
+
+directors = films.map(&:director).uniq
+
+puts 'Программа "Фильм на вечер"'
+puts
+
+directors.each.with_index(1) do |director, index|
+  puts "#{index}: #{director}"
+end
+
+puts 'Фильм какого режиссера вы хотите сегодня посмотреть?'
+user_choise = STDIN.gets.to_i
+
+if user_choise <= directors.size
+  result = films.select do |film|
+    film.director == directors[user_choise - 1]
+  end.sample
+  puts 'И сегодня вечером рекомендую посмотреть:'
+  puts result
+else
+  puts "Введите число в диапазоне от 1 до #{directors.size}!"
+end
